2d246f1e92e071626b82826d784b24f13a51b84c
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-11-06 06:19:30 -0700

    feat: reduced index.css size by using 'gulpPurgeCSS'
    gulpPurgeCSS will look through the html file and only add the classes that are being used to the output CSS file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const { src, dest, watch, series } = require("gulp");
+const gulpPurgeCSS = require("gulp-purgecss");
 const sass = require("gulp-sass")(require("sass"));
 
 // Compiling SCSS to CSS
 function buildStyles() {
   return src("shinobi/**/*.scss")
     .pipe(sass().on("error", sass.logError))
+    .pipe(gulpPurgeCSS({ content: ["*.html"] }))
     .pipe(dest("css"));
 }
 
